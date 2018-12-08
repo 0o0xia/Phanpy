59c2c96744a8d653a3e320dc2e1795af4d8bc724
Bei Li <kylin.roc@me.com>
2018-12-08 22:47:39 +0800

    Add status table view cell

---
+++ Phanpy/StatusTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  StatusTableViewCell.swift
+//  Phanpy
+//
+//  Created by 李孛 on 2018/12/8.
+//
+
+import UIKit
+
+class StatusTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
