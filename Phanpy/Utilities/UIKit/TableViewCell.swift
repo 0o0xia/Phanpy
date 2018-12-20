ef2683cab93b35c15c1b22b9f45ad6574e08ff9a
Bei Li <kylin.roc@me.com>
2018-12-20 12:03:48 +0800

    Update

--- Phanpy/Utilities/UIKit/TableViewCell.swift
+++ Phanpy/Utilities/UIKit/TableViewCell.swift
@@ -10,6 +10,10 @@ import UIKit
 class TableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        // https://pspdfkit.com/blog/2018/self-sizing-table-view-cells/#margins
+        // http://openradar.appspot.com/46234942
+        contentView.preservesSuperviewLayoutMargins = true
     }
 
     @available(*, unavailable)
